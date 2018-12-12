2e0c2a2eccdb87600b788f4def7887732620d34e
Samuel Becerra <samuel.becerra@e3civichigh.com>
2018-12-12 10:21:19 -0800

    Corrected two for loops

--- src/com/e3civichigh/coding/Factorial.java
+++ src/com/e3civichigh/coding/Factorial.java
@@ -5,27 +5,22 @@ public class Factorial
     public static void main(String[] args)
     {
         final int NUM_FACTS = 100;
-
         // TODO 1: Please create a for loop that counts from 0 up to NUM_FACTS
-        for () {
-            System.out.println(i + "! is " + factorial(i));
-        }
+        for (int i=0; i <= NUM_FACTS; i++)
+            {
+                System.out.println(i + "! is " + factorial(i));
+            }
     }
 
-    /**
-     * factorial computes n!
-     *
-     * @param n
-     * @return n factorial
-     */
     public static int factorial(int n)
     {
         int result = 1;
 
-        // TODO 2: Please create a foor loop that counts from 2 up to and including n
-        for() {
-            result *= i;
-        }
+        // TODO 2: Please create a for loop that counts from 2 up to and including n
+        for(int i=2; i <= n; i++)
+            {
+                result *= i;
+            }
 
         return result;
     }
